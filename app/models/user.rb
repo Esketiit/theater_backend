4979b037b9a217b409cee54106481a23df0a01fa
github username <heftygac@gmail.com>
2020-10-02 19:29:11 -0400

    working on sessions controller and fixing bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
     has_many :comments
+    
+    has_secure_password 
+
+    validates_presence_of :username
+    validates_uniqueness_of :username
+
+    # Saving emails for later 
+    # validates presence_of :email
+    # validates uniqueness_of :email
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   resources :user
   resources :user_room, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
+  # sessions route for authentications and cookie creation
+  resources :sessions, only: [:create]
+  # registrations route that allows users to create accounts
+  resources :registrations, only: [:create]
+  root to: "static#home"
 end
